fead26a74bc8443e2e45c67c07596634f0478889
Dr-Cyanide <103555287+Dr-Cyanide@users.noreply.github.com>
2022-05-05 12:08:45 +1000

    Given assets colliders

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{
+    public int level = 0;
+    
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //level = whatlevel;
+            //SceneManager.GetSceneByName(whatlevel);
+            SceneManager.LoadScene(level);
+        }
+    }
+    
+}
