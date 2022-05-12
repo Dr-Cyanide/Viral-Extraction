6c2a547d27be64477c597b727e7dedc8ddf2a2ac
Dr-Cyanide <103555287+Dr-Cyanide@users.noreply.github.com>
2022-05-12 14:56:55 +1000

    Added and scripted medkits

---
+++ Assets/Scripts/Medpack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medpack : MonoBehaviour
+{
+    public float HealAmount;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth>().playerHealth += HealAmount;
+            if (other.gameObject.GetComponent<PlayerHealth>().playerHealth > 100)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().playerHealth = 100;   
+            }
+            Destroy(gameObject);
+        }
+    }
+}
