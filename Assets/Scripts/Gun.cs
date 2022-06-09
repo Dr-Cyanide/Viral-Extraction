f0cd44861dc3165ca9dea9be94890b6004db6b58
Dr-Cyanide <103555287+Dr-Cyanide@users.noreply.github.com>
2022-06-09 12:05:28 +1000

    Partly finished new gun script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Gun : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class Gun : MonoBehaviour
     GameObject bulletSpawnPoint;
     public bool canFire;
     public float rateOfFire;
+    public TextMeshProUGUI ammoInfoText;
+
+    public int currentClipAmmo;
+    public int maxAmmo = 15;
+    public int reserveSize = 25;
+
+    public float reloadTime = 1.5f;
+    private bool isReloading;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +25,32 @@ public class Gun : MonoBehaviour
         bulletSpawnPoint = transform.GetChild(0).gameObject;
         canFire = true;
 
+        currentClipAmmo = maxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        fireShot();
-    }
+        if(currentClipAmmo == 0 && reserveSize == 0)
+        {
+            canFire = false;
+            return;
+        }
+        if (isReloading)
+            return;
 
-    public void fireShot()
+        Gun currentgun = FindObjectOfType<Gun>();
+        ammoInfoText.text = currentgun.currentClipAmmo + " / " + currentgun.reserveSize;
+        Shoot();
+
+        if(currentClipAmmo == 0 && !isReloading)
+        {
+            StartCoroutine(Reload());
+        }
+    } 
+    
+
+    public void Shoot()
     {
 
         if (Input.GetMouseButtonDown(0) && canFire)
@@ -38,8 +64,26 @@ public class Gun : MonoBehaviour
             GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
             yield return new WaitForSeconds(rateOfFire);
             canFire = true;
-
+            currentClipAmmo--;
         }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        yield return new WaitForSeconds(reloadTime);
+        if(reserveSize >= maxAmmo)
+        {
+            currentClipAmmo = maxAmmo;
+            reserveSize -= maxAmmo;
+        }
+        else
+        {
+            currentClipAmmo = reserveSize;
+            reserveSize = 0;  
+        }
+        isReloading = false;
     }
 }
         
